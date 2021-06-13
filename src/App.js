9fac72bcccf1ea108b284d4c529579eb1c7195d5
Ash-desig <gargashi200@gmail.com>
2021-06-13 12:48:45 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React from "react";
 import "./style.css";
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      price:999,
+      title: 'Phone',
+      qty: 1,
+      img: ''
+    }
+  }
   render(){
+    const{price,title,qty}=this.state
     return (
       <div className="App">
         <h1>Shopping Cart</h1>
@@ -11,9 +21,9 @@ export default class App extends React.Component {
             <img style={styles.image}/> 
           </div>
           <div className="right-block">
-            <div style={ { fontSize:25 } }>Phone</div>
-            <div style={ { color:'#777' } }>Rs 999</div>
-            <div style={ { color:'#777' } }>Qty: 1</div>
+            <div style={ { fontSize:25 } }>{title}</div>
+            <div style={ { color:'#777' } }>Rs {price}</div>
+            <div style={ { color:'#777' } }>Qty: {qty}</div>
             <div className="cart-item-actions">
               {/*buttons*/}
               <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/128/992/992651.png" />
