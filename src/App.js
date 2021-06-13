0b4ac52b6e6d9115d760469ebff19ced56417580
Ash-desig <gargashi200@gmail.com>
2021-06-13 12:36:51 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-    </div>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Shopping Cart</h1>
+        <div className="cart-item">
+          <div className="left-block">
+            <img style={styles.image}/> 
+          </div>
+          <div className="right-block">
+            <div style={ { fontSize:25 } }>Phone</div>
+            <div style={ { color:'#777' } }>Rs 999</div>
+            <div style={ { color:'#777' } }>Qty: 1</div>
+            <div className="cart-item-actions">
+              {/*buttons*/}
+              <img alt="increase" className="action-icons" src="https://image.flaticon.com/icons/png/128/992/992651.png" />
+              <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/png/512/992/992683.png" />
+              <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/png/128/3096/3096673.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles={
+  image:{
+    height:110,
+    width:110,
+    borderRadius:4,
+    background:'#ccc'
+  }
 }
